cee076126e28d2c9c7d2a58c028aa48619c92344
Jerry Gamblin <jerry.gamblin@gmail.com>
2015-05-29 10:37:19 -0500

    Create crash2.py

---
+++ crash2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+# Name:     crash.py
+# Purpose:  Crashes OSX Terminal
+# By:       Jerry Gamblin
+# Date:     27.05.15
+# Modified  27.05.15
+# Rev Level 0.5
+# -----------------------------------------------
+ 
+print'פריצה ॣ ॣ  ꣶ ॣ ॣ\n 冗'
